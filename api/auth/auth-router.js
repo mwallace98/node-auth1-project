a53f41fe9967cba7008d52b55398aece5c5c6af6
mwallace98 <mattwallace140@gmail.com>
2024-01-31 20:48:03 -0500

    restricted middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -63,7 +63,7 @@ const {
     const {password} = req.body
     if(bcrypt.compareSync(password, req.user.password)){
       req.session.user = req.user
-      res.json({message: `Welcome${req.user.username}!`})
+      res.json({message: `Welcome ${req.user.username}!`})
     }else{
       next({status:401, message: "Invalid credentials"})
     }
